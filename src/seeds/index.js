b129c2c84898adcc3c88e6a10105554c60db202e
Kayle Rieger Patton <kayle.patton22@gmail.com>
2022-04-05 21:44:53 +0100

    seed loops data

--- src/seeds/index.js
+++ src/seeds/index.js
@@ -4,7 +4,8 @@ const mongoose = require("mongoose");
 const { Character, Loop, Peculiarity, Book } = require("../models");
 
 // import data files
-// const books = require("./data/books.json");
+const books = require("./data/books.json");
+const loops = require("./data/loops.json");
 
 const seed = async () => {
   try {
@@ -25,102 +26,106 @@ const seed = async () => {
     await Book.deleteMany({});
 
     await Book.insertMany(books);
-    console.log("[INFO]: Books seeded successfully");
+    console.log("[INFO]: Books seeded successfully.");
 
-    const universitiesFromDb = await University.find({});
+    // currently seeding without ymbryne Character reference
+    await Loop.insertMany(loops);
+    console.log("[INFO]: Loops seeded successfully.");
 
-    const staffToSeed = staffs.map((staff) => {
-      return {
-        ...staff,
-        university:
-          universitiesFromDb[randomIndex(universitiesFromDb.length)]._id,
-      };
-    });
+    // const universitiesFromDb = await University.find({});
 
-    const staffPromises = staffToSeed.map((staff) => {
-      return Staff.create(staff);
-    });
+    // const staffToSeed = staffs.map((staff) => {
+    //   return {
+    //     ...staff,
+    //     university:
+    //       universitiesFromDb[randomIndex(universitiesFromDb.length)]._id,
+    //   };
+    // });
 
-    await Promise.all(staffPromises);
+    // const staffPromises = staffToSeed.map((staff) => {
+    //   return Staff.create(staff);
+    // });
 
-    console.log("[INFO]: Staffs seeded successfully");
+    // await Promise.all(staffPromises);
 
-    const staffFromDb = await Staff.find({});
-    const jobsToSeed = jobs.map((job) => {
-      return {
-        ...job,
-        postedBy: staffFromDb[randomIndex(staffFromDb.length)]._id,
-        closingDate: new Date(),
-      };
-    });
+    // console.log("[INFO]: Staffs seeded successfully");
 
-    await Job.insertMany(jobsToSeed);
-    console.log("[INFO]: Jobs seeded successfully");
+    // const staffFromDb = await Staff.find({});
+    // const jobsToSeed = jobs.map((job) => {
+    //   return {
+    //     ...job,
+    //     postedBy: staffFromDb[randomIndex(staffFromDb.length)]._id,
+    //     closingDate: new Date(),
+    //   };
+    // });
 
-    const jobsFromDb = await Job.find({});
+    // await Job.insertMany(jobsToSeed);
+    // console.log("[INFO]: Jobs seeded successfully");
 
-    const studentsToSeed = students.map((student) => {
-      return {
-        ...student,
-        university:
-          universitiesFromDb[randomIndex(universitiesFromDb.length)]._id,
-        savedJobs: [jobsFromDb[randomIndex(jobsFromDb.length)]._id],
-      };
-    });
+    // const jobsFromDb = await Job.find({});
 
-    const studentPromises = studentsToSeed.map((student) => {
-      return Student.create(student);
-    });
+    // const studentsToSeed = students.map((student) => {
+    //   return {
+    //     ...student,
+    //     university:
+    //       universitiesFromDb[randomIndex(universitiesFromDb.length)]._id,
+    //     savedJobs: [jobsFromDb[randomIndex(jobsFromDb.length)]._id],
+    //   };
+    // });
 
-    await Promise.all(studentPromises);
-    console.log("[INFO]: Students seeded successfully");
+    // const studentPromises = studentsToSeed.map((student) => {
+    //   return Student.create(student);
+    // });
 
-    const studentsFromDb = await Student.find({});
+    // await Promise.all(studentPromises);
+    // console.log("[INFO]: Students seeded successfully");
 
-    const finalTransactions = transactions.map((transaction) => {
-      return {
-        ...transaction,
-        buyer: studentsFromDb[randomIndex(studentsFromDb.length)]._id,
-        collectionDate: new Date(),
-      };
-    });
+    // const studentsFromDb = await Student.find({});
 
-    const commentsToSeed = comments.map((comment) => {
-      return {
-        ...comment,
-        username: studentsFromDb[randomIndex(studentsFromDb.length)]._id,
-      };
-    });
+    // const finalTransactions = transactions.map((transaction) => {
+    //   return {
+    //     ...transaction,
+    //     buyer: studentsFromDb[randomIndex(studentsFromDb.length)]._id,
+    //     collectionDate: new Date(),
+    //   };
+    // });
 
-    const itemsToSeed = items.map((item) => {
-      return {
-        ...item,
-        transactions: finalTransactions,
-        comments: commentsToSeed,
-        seller: studentsFromDb[randomIndex(studentsFromDb.length)]._id,
-      };
-    });
+    // const commentsToSeed = comments.map((comment) => {
+    //   return {
+    //     ...comment,
+    //     username: studentsFromDb[randomIndex(studentsFromDb.length)]._id,
+    //   };
+    // });
 
-    await Item.insertMany(itemsToSeed);
-    console.log("[INFO]: Items seeded successfully");
+    // const itemsToSeed = items.map((item) => {
+    //   return {
+    //     ...item,
+    //     transactions: finalTransactions,
+    //     comments: commentsToSeed,
+    //     seller: studentsFromDb[randomIndex(studentsFromDb.length)]._id,
+    //   };
+    // });
 
-    const forumRepliesToSeed = forumReplies.map((reply) => {
-      return {
-        ...reply,
-        user: studentsFromDb[randomIndex(studentsFromDb.length)]._id,
-      };
-    });
+    // await Item.insertMany(itemsToSeed);
+    // console.log("[INFO]: Items seeded successfully");
 
-    const forumPostsToSeed = forumPosts.map((forumPost) => {
-      return {
-        ...forumPost,
-        replies: forumRepliesToSeed,
-        postedBy: studentsFromDb[randomIndex(studentsFromDb.length)]._id,
-      };
-    });
+    // const forumRepliesToSeed = forumReplies.map((reply) => {
+    //   return {
+    //     ...reply,
+    //     user: studentsFromDb[randomIndex(studentsFromDb.length)]._id,
+    //   };
+    // });
 
-    await ForumPost.insertMany(forumPostsToSeed);
-    console.log("[INFO]: ForumPosts seeded successfully");
+    // const forumPostsToSeed = forumPosts.map((forumPost) => {
+    //   return {
+    //     ...forumPost,
+    //     replies: forumRepliesToSeed,
+    //     postedBy: studentsFromDb[randomIndex(studentsFromDb.length)]._id,
+    //   };
+    // });
+
+    // await ForumPost.insertMany(forumPostsToSeed);
+    // console.log("[INFO]: ForumPosts seeded successfully");
   } catch (error) {
     console.log(`[ERROR]: Database connection failed | ${error.message}`);
   }
