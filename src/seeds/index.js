f49dd499dc0f507dcde3a774a2c5f3cb4c2208dd
Kayle Rieger Patton <kayle.patton22@gmail.com>
2022-04-17 15:43:54 +0100

    seed Fiona, Victor peculiarities

--- src/seeds/index.js
+++ src/seeds/index.js
@@ -1,12 +1,13 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-const { Character, Loop, Book } = require("../models");
+const { Character, Loop, Book, Peculiarity } = require("../models");
 
 // import data files
 const books = require("./data/books.json");
 const loops = require("./data/loops.json");
 const characters = require("./data/characters.json");
+const peculiarities = require("./data/peculiarities.json");
 
 const seed = async () => {
   try {
@@ -24,35 +25,43 @@ const seed = async () => {
     await Character.deleteMany({});
     await Loop.deleteMany({});
     await Book.deleteMany({});
+    await Peculiarity.deleteMany({});
 
     await Book.insertMany(books);
     console.log("[INFO]: Books seeded successfully.");
 
-    //* currently seeding without ymbryne Character reference; TODO: after seeding characters, get ymbryne character, update loop record with character id
+    await Peculiarity.insertMany(peculiarities);
+    console.log("[INFO]: Peculiarities seeded successfully.");
 
+    //* currently seeding LOOPS without ymbryne Character reference; TODO: after seeding characters, get ymbryne character, update loop record with the character id
     await Loop.insertMany(loops);
     console.log("[INFO]: Loops seeded successfully.");
 
-    // get loop & books
+    // get loop, books, peculiarities
     const seededLoop = await Loop.find({});
     const seededBooks = await Book.find({});
     console.log(seededBooks);
+    const seededPeculiarities = await Peculiarity.find({});
 
-    // iterate through characters, add the loop id to homeLoop path, books reference, and peculiarities
+    console.log(seededPeculiarities);
+
+    // iterate through characters, add the loop id to homeLoop path, books reference, and peculiarities reference
     const charactersToSeed = characters.map((character) => {
       // if Fiona, don't add #6
       if (character.name === "Fiona Frauenfeld") {
         return {
           ...character,
+          peculiarity: seededPeculiarities[9]._id,
           homeLoop: seededLoop[0]._id,
           books: seededBooks.splice(5, 1),
         };
       }
 
-      // if victor, only add #1
+      // if victor, only add book #1
       if (character.name === "Victor Bruntley") {
         return {
           ...character,
+          peculiarity: seededPeculiarities[6]._id,
           homeLoop: seededLoop[0]._id,
           books: seededBooks[0]._id,
         };
