1699a6206aa0611e1a6e297c3da13ade30ee93ab
Kayle Patton <kayle.patton@bbc.co.uk>
2022-09-02 09:31:50 +0100

    add book 8 to seed data & make Book model an enum

--- src/models/Book.js
+++ src/models/Book.js
@@ -4,12 +4,21 @@ const { Schema, model } = require("mongoose");
 const bookSchema = {
   title: {
     type: String,
-    required: true,
+    enum: [
+      "Miss Peregrine's Home for Peculiar Children",
+      "Hollow City",
+      "Library of Souls",
+      "Tales of the Peculiar",
+      "A Map of Days",
+      "The Conference of the Birds",
+      "The Desolation of Devil's Acre",
+      "Miss Peregrine's Museum of Wonders: An Indispensable Guide to the Dangers and Delights of the Peculiar World for the Instruction of New Arrivals"
+    ]
   },
 
   author: {
     type: String,
-    required: true,
+    default: "Ransom Riggs"
   },
 
   releaseYear: {
