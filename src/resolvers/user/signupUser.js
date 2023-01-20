55cf0dcbec34125451c25b0cfe29fa2d72132130
Kayle Patton <kayle.patton@bbc.co.uk>
2023-01-20 11:05:45 +0000

    add login & signup user resolvers

---
+++ src/resolvers/user/signupUser.js
@@ -0,0 +1,14 @@
+const { ApolloError } = require('apollo-server');
+const { User } = require('../../models')
+
+const signup = async (_, { input }) => {
+  try {
+    const user = await User.create(input)
+    return { success: true, user }
+  } catch (error) {
+    console.log(`[ERROR]: Failed to sign up | ${error.message}`);
+    throw new ApolloError("Failed to sign up.")
+  }
+}
+
+module.exports = signup
